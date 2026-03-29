3771290425be6b30caac96a7633ff36a6069f0ef
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2026-03-30 01:16:17 +0300

    Added maximization checks and some basic game structs.

--- src/config.rs
+++ src/config.rs
@@ -1,2 +1,6 @@
 pub const SNEK_BACKGROUND_COLOR: [f32; 3] = [0.188, 0.188, 0.188];
+pub const GRID_LINE_COLOR: [f32; 3] = [0.5, 0.5, 0.5];
+
+pub const SNAKE_SPAWN_AREA_MARGIN: f32 = 0.3;
+
 pub const TILE_SIZE: f32 = 50.0;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 pub mod config;
 
-use bevy::{prelude::*, window::WindowResized};
+use std::collections::HashMap;
+
+use bevy::{
+    prelude::*,
+    window::{EnabledButtons, WindowResized},
+};
 
 use crate::config::*;
 
 #[derive(Resource)]
-struct MainWindowDesc {
+pub struct MainWindowDesc {
     pub width: f32,
     pub height: f32,
     pub id: Option<Entity>,
@@ -17,6 +22,95 @@ impl MainWindowDesc {
             height: 0.0,
             id: None,
         }
+    }
+}
+
+pub struct MaximizeShenanigans {
+    pub last_known_size: u64,
+    pub assume_maximize_happened: bool,
+}
+impl MaximizeShenanigans {
+    pub fn new() -> Self {
+        Self {
+            last_known_size: 0,
+            assume_maximize_happened: false,
+        }
+    }
+}
+
+#[derive(Component, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Component, Clone)]
+pub struct SnakeSegment {
+    pub pos: BoardPos,
+    pub next: Option<BoardPos>,
+    pub prev: Option<BoardPos>,
+}
+
+#[derive(Component, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct BoardPos {
+    pub x: u32,
+    pub y: u32,
+}
+
+#[derive(Component, Clone)]
+pub struct Snake {
+    pub snake_segments: HashMap<BoardPos, SnakeSegment>,
+    pub head_pos: BoardPos,
+    pub tail_pos: BoardPos,
+    pub direction: Direction,
+}
+impl Snake {
+    pub fn new(start_pos: BoardPos) -> Self {
+        Self {
+            snake_segments: HashMap::from([(
+                start_pos,
+                SnakeSegment {
+                    pos: start_pos,
+                    next: None,
+                    prev: None,
+                },
+            )]),
+            head_pos: start_pos,
+            tail_pos: start_pos,
+            direction: Direction::Right,
+        }
+    }
+}
+
+#[derive(Resource)]
+pub struct Board {
+    pub maximize_shenanigans: MaximizeShenanigans,
+    column_count: u32,
+    row_count: u32,
+    snake: Option<Snake>,
+}
+
+impl Board {
+    pub fn new() -> Self {
+        Self {
+            maximize_shenanigans: MaximizeShenanigans::new(),
+            column_count: 0,
+            row_count: 0,
+            snake: None,
+        }
+    }
+    pub fn set_size_once(&mut self, cols: u32, rows: u32) {
+        self.column_count = cols;
+        self.row_count = rows;
+    }
+
+    pub fn cols(&self) -> u32 {
+        self.column_count
+    }
+    pub fn rows(&self) -> u32 {
+        self.row_count
     }
 }
 
@@ -35,20 +129,91 @@ fn init_game(
 fn on_window_resized(
     mut reader: MessageReader<WindowResized>,
     mut window_desc: ResMut<MainWindowDesc>,
+    mut board: ResMut<Board>,
+    mut window: Query<&mut Window>,
 ) {
     for e in reader.read() {
         if let Some(desc_ent) = window_desc.id {
             if e.window == desc_ent {
+                let current_size = e.width as u64 * e.height as u64;
+                if board.maximize_shenanigans.last_known_size != 0
+                    && board.maximize_shenanigans.last_known_size < current_size
+                {
+                    board.maximize_shenanigans.assume_maximize_happened = true;
+                    let mut w = window.get_mut(desc_ent).unwrap();
+                    w.resizable = false;
+                    w.enabled_buttons = EnabledButtons {
+                        maximize: false,
+                        ..default()
+                    };
+                }
+                board.maximize_shenanigans.last_known_size = current_size;
                 window_desc.width = e.width;
                 window_desc.height = e.height;
+                board.set_size_once(
+                    e.width as u32 / TILE_SIZE as u32,
+                    e.height as u32 / TILE_SIZE as u32,
+                );
             }
         }
     }
 }
 
+fn draw_grid_lines(mut gizmos: Gizmos, window_desc: ResMut<MainWindowDesc>) {
+    let column_count: u32 = window_desc.width as u32 / TILE_SIZE as u32;
+    let row_count: u32 = window_desc.height as u32 / TILE_SIZE as u32;
+
+    gizmos
+        .grid_2d(
+            Isometry2d::IDENTITY,
+            UVec2::new(column_count, row_count),
+            Vec2::splat(TILE_SIZE),
+            Color::srgb_from_array(GRID_LINE_COLOR),
+        )
+        .outer_edges();
+}
+
+fn is_maximized(board: Res<Board>) -> bool {
+    board.maximize_shenanigans.assume_maximize_happened
+}
+
+// TODO: actually spawn the goddamn snake!!
+fn spawn_snake(mut board: &mut ResMut<Board>) {
+    let x_center = board.cols() / 2;
+    let y_center = board.rows() / 2;
+    let offset_from_center = SNAKE_SPAWN_AREA_MARGIN / 2.;
+    let offset_x = (board.cols() as f32 * offset_from_center).floor() as u32;
+    let offset_y = (board.rows() as f32 * offset_from_center).floor() as u32;
+    let x_start = x_center - offset_x;
+    let y_start = y_center - offset_y;
+    let x_end = x_center + offset_x;
+    let y_end = y_center + offset_y;
+
+    warn!(
+        "spawn_snake(): board: {}x{}, offset x: {}, offset y: {}, center: {}x{}, spawn from: ({}, {}) to ({}, {})",
+        board.cols(),
+        board.rows(),
+        offset_x,
+        offset_y,
+        x_center,
+        y_center,
+        x_start,
+        y_start,
+        x_end,
+        y_end
+    );
+}
+fn update_snake(mut board: ResMut<Board>) {
+    if board.snake.is_none() {
+        spawn_snake(&mut board);
+    }
+    error!("BOARD SIZE: {}x{}!", board.cols(), board.rows());
+}
+
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::srgb_from_array(SNEK_BACKGROUND_COLOR)))
+        .insert_resource(Board::new())
         .insert_resource(MainWindowDesc::new())
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
@@ -60,5 +225,7 @@ fn main() {
         }))
         .add_systems(Startup, init_game)
         .add_systems(Update, on_window_resized)
+        .add_systems(Update, update_snake.run_if(is_maximized))
+        .add_systems(Update, draw_grid_lines)
         .run();
 }
