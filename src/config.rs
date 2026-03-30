9978af28647c919e48186e20ff2d72aa1dd6b59a
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2026-03-30 07:28:36 +0300

    Now putting a snake segment/head on board at random pos in central area of screen.

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,8 @@
 pub const SNEK_BACKGROUND_COLOR: [f32; 3] = [0.188, 0.188, 0.188];
 pub const GRID_LINE_COLOR: [f32; 3] = [0.5, 0.5, 0.5];
+pub const SNEK_SEGMENT_COLOR: [f32; 3] = [1.0, 1.0, 1.0];
 
 pub const SNAKE_SPAWN_AREA_MARGIN: f32 = 0.3;
 
 pub const TILE_SIZE: f32 = 50.0;
+pub const MARGIN_SIZE: f32 = 2.0;
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,31 @@ use std::collections::HashMap;
 
 use bevy::{
     prelude::*,
-    window::{EnabledButtons, WindowResized},
+    window::{EnabledButtons, PrimaryWindow, WindowResized},
 };
+use rand::prelude::*;
 
 use crate::config::*;
 
-#[derive(Resource)]
+#[derive(Component, Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub struct BoardPos {
+    pub x: u32,
+    pub y: u32,
+}
+
+fn board_pos_to_world(pos: &BoardPos, board: &Board) -> (f32, f32) {
+    let grid_width = board.cols() as f32 * TILE_SIZE;
+    let grid_height = board.rows() as f32 * TILE_SIZE;
+
+    // Tile center in top-left-origin space
+    let x = pos.x as f32 * TILE_SIZE + TILE_SIZE / 2.;
+    let y = pos.y as f32 * TILE_SIZE + TILE_SIZE / 2.;
+
+    // Convert to Bevy's center-origin, Y-up
+    (x - grid_width / 2., grid_height / 2. - y)
+}
+
+#[derive(Resource, Debug)]
 pub struct MainWindowDesc {
     pub width: f32,
     pub height: f32,
@@ -21,6 +40,29 @@ impl MainWindowDesc {
             width: 0.0,
             height: 0.0,
             id: None,
+        }
+    }
+}
+
+#[derive(Resource)]
+pub struct GameRng {
+    pub rng: rand::rngs::StdRng,
+}
+impl GameRng {
+    pub fn new() -> Self {
+        Self {
+            rng: rand::make_rng(),
+        }
+    }
+
+    pub fn random_pos(
+        &mut self,
+        x_range: impl rand::distr::uniform::SampleRange<u32>,
+        y_range: impl rand::distr::uniform::SampleRange<u32>,
+    ) -> BoardPos {
+        BoardPos {
+            x: self.rng.random_range(x_range),
+            y: self.rng.random_range(y_range),
         }
     }
 }
@@ -53,34 +95,60 @@ pub struct SnakeSegment {
     pub prev: Option<BoardPos>,
 }
 
-#[derive(Component, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct BoardPos {
-    pub x: u32,
-    pub y: u32,
-}
-
 #[derive(Component, Clone)]
 pub struct Snake {
-    pub snake_segments: HashMap<BoardPos, SnakeSegment>,
+    pub snake_segments: HashMap<BoardPos, Entity>,
     pub head_pos: BoardPos,
     pub tail_pos: BoardPos,
     pub direction: Direction,
 }
 impl Snake {
-    pub fn new(start_pos: BoardPos) -> Self {
-        Self {
-            snake_segments: HashMap::from([(
-                start_pos,
-                SnakeSegment {
-                    pos: start_pos,
-                    next: None,
-                    prev: None,
-                },
-            )]),
+    pub fn new(
+        start_pos: BoardPos,
+        commands: Commands,
+        meshes: ResMut<Assets<Mesh>>,
+        materials: ResMut<Assets<ColorMaterial>>,
+        board: &Board,
+    ) -> Self {
+        let mut snake = Self {
+            snake_segments: HashMap::new(),
             head_pos: start_pos,
             tail_pos: start_pos,
             direction: Direction::Right,
+        };
+        let _ = snake.spawn_segment(commands, meshes, materials, board);
+        snake
+    }
+
+    pub fn spawn_segment(
+        &mut self,
+        mut commands: Commands,
+        mut meshes: ResMut<Assets<Mesh>>,
+        mut materials: ResMut<Assets<ColorMaterial>>,
+        board: &Board,
+    ) -> Entity {
+        let is_head = self.snake_segments.is_empty();
+        let mut entity = Entity::from_raw_u32(0).unwrap();
+        match is_head {
+            true => {
+                let head_xy = board_pos_to_world(&self.head_pos, board);
+                entity = commands
+                    .spawn((
+                        Mesh2d(meshes.add(Rectangle::new(TILE_SIZE, TILE_SIZE))),
+                        MeshMaterial2d(materials.add(Color::srgb_from_array(SNEK_SEGMENT_COLOR))),
+                        Transform::from_xyz(head_xy.0, head_xy.1, 0.0),
+                        SnakeSegment {
+                            pos: self.head_pos,
+                            next: None,
+                            prev: None,
+                        },
+                    ))
+                    .id();
+                self.snake_segments.insert(self.head_pos, entity);
+            }
+            false => (),
         }
+        entity
     }
 }
 
@@ -118,7 +186,7 @@ fn init_game(
     mut window_res: ResMut<MainWindowDesc>,
     mut commands: Commands,
     mut window: Single<&mut Window>,
-    window_ent: Query<Entity, With<Window>>,
+    window_ent: Query<Entity, With<PrimaryWindow>>,
 ) {
     let win_ent = window_ent.single().unwrap();
     commands.spawn(Camera2d);
@@ -178,41 +246,43 @@ fn is_maximized(board: Res<Board>) -> bool {
 }
 
 // TODO: actually spawn the goddamn snake!!
-fn spawn_snake(mut board: &mut ResMut<Board>) {
+fn spawn_snake(
+    board: &mut Board,
+    rng: &mut ResMut<GameRng>,
+    commands: Commands,
+    meshes: ResMut<Assets<Mesh>>,
+    materials: ResMut<Assets<ColorMaterial>>,
+) {
     let x_center = board.cols() / 2;
     let y_center = board.rows() / 2;
     let offset_from_center = SNAKE_SPAWN_AREA_MARGIN / 2.;
-    let offset_x = (board.cols() as f32 * offset_from_center).floor() as u32;
-    let offset_y = (board.rows() as f32 * offset_from_center).floor() as u32;
+    let offset_x = (board.cols() as f32 * offset_from_center) as u32;
+    let offset_y = (board.rows() as f32 * offset_from_center) as u32;
     let x_start = x_center - offset_x;
     let y_start = y_center - offset_y;
     let x_end = x_center + offset_x;
     let y_end = y_center + offset_y;
+    let snake_pos = rng.random_pos(x_start..=x_end, y_start..=y_end);
 
-    warn!(
-        "spawn_snake(): board: {}x{}, offset x: {}, offset y: {}, center: {}x{}, spawn from: ({}, {}) to ({}, {})",
-        board.cols(),
-        board.rows(),
-        offset_x,
-        offset_y,
-        x_center,
-        y_center,
-        x_start,
-        y_start,
-        x_end,
-        y_end
-    );
+    board.snake = Some(Snake::new(snake_pos, commands, meshes, materials, board));
+    error!("Put snake at {:?}!", snake_pos);
 }
-fn update_snake(mut board: ResMut<Board>) {
+fn update_snake(
+    mut board: ResMut<Board>,
+    mut rng: ResMut<GameRng>,
+    commands: Commands,
+    meshes: ResMut<Assets<Mesh>>,
+    materials: ResMut<Assets<ColorMaterial>>,
+) {
     if board.snake.is_none() {
-        spawn_snake(&mut board);
+        spawn_snake(&mut board, &mut rng, commands, meshes, materials);
     }
-    error!("BOARD SIZE: {}x{}!", board.cols(), board.rows());
 }
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::srgb_from_array(SNEK_BACKGROUND_COLOR)))
+        .insert_resource(GameRng::new())
         .insert_resource(Board::new())
         .insert_resource(MainWindowDesc::new())
         .add_plugins(DefaultPlugins.set(WindowPlugin {
