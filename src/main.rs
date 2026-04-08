fd88020f66aec1eda2deb4a4ad07801f87a769d7
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2026-04-09 00:07:45 +0300

    Implemented snake movement. Weirdly enough, works first try. TODO: collision with apple.

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ use crate::config::*;
 
 #[derive(Component, Clone, Copy, PartialEq, Eq, Hash, Debug)]
 pub struct BoardPos {
-    pub x: u32,
-    pub y: u32,
+    pub x: i32,
+    pub y: i32,
 }
 
 #[derive(Component, Clone, Debug, PartialEq, Eq)]
@@ -75,12 +75,23 @@ impl GameRng {
 
     pub fn random_pos(
         &mut self,
-        x_range: impl rand::distr::uniform::SampleRange<u32>,
-        y_range: impl rand::distr::uniform::SampleRange<u32>,
+        x_range: impl rand::distr::uniform::SampleRange<i32>,
+        y_range: impl rand::distr::uniform::SampleRange<i32>,
     ) -> BoardPos {
         BoardPos {
             x: self.rng.random_range(x_range),
             y: self.rng.random_range(y_range),
+        }
+    }
+
+    pub fn random_direction(&mut self) -> Direction {
+        let random_int_dir = self.rng.random_range(0..3);
+        match random_int_dir {
+            0 => Direction::Up,
+            1 => Direction::Right,
+            2 => Direction::Down,
+            3 => Direction::Left,
+            _ => panic!("GameRng::random_direction(): 5th direction picked???"),
         }
     }
 }
@@ -104,6 +115,7 @@ pub struct SnakeSegment {
     pub pos: BoardPos,
     pub next: Option<BoardPos>,
     pub prev: Option<BoardPos>,
+    pub direction: Direction,
 }
 
 #[derive(Component, Clone)]
@@ -119,13 +131,14 @@ impl Snake {
         commands: &mut Commands,
         meshes: &mut ResMut<Assets<Mesh>>,
         materials: &mut ResMut<Assets<ColorMaterial>>,
+        rng: &mut ResMut<GameRng>,
         board: &Board,
     ) -> Self {
         let mut snake = Self {
             snake_segments: HashMap::new(),
             head_pos: start_pos,
             tail_pos: start_pos,
-            direction: Direction::Right,
+            direction: rng.random_direction(),
         };
         let _ = snake.spawn_segment(commands, meshes, materials, board);
         snake
@@ -152,6 +165,7 @@ impl Snake {
                             pos: self.head_pos,
                             next: None,
                             prev: None,
+                            direction: Direction::Right,
                         },
                     ))
                     .id();
@@ -161,12 +175,17 @@ impl Snake {
         }
         entity
     }
+
+    pub fn segment_by_pos(&self, pos: BoardPos) -> Entity {
+        let ent = self.snake_segments.get(&pos).unwrap();
+        *ent
+    }
 }
 
 #[derive(Component, Clone, Debug)]
 pub struct Apple;
 
-// FIXME: Board should not hold references to snake, apple, or anything else. Rely on Bevy's ECS for that.
+// FIXME: Board should not hold references to snake, apple, or anything else. Rely on Bevy's ECS for that. Decouple board (maximize + column/row counts)
 #[derive(Resource, Debug)]
 pub struct Board {
     pub maximize_shenanigans: MaximizeShenanigans,
@@ -277,10 +296,10 @@ fn spawn_snake(
     let offset_from_center = SNAKE_SPAWN_AREA_MARGIN / 2.;
     let offset_x = (board.cols() as f32 * offset_from_center) as u32;
     let offset_y = (board.rows() as f32 * offset_from_center) as u32;
-    let x_start = x_center - offset_x;
-    let y_start = y_center - offset_y;
-    let x_end = x_center + offset_x;
-    let y_end = y_center + offset_y;
+    let x_start = (x_center - offset_x) as i32;
+    let y_start = (y_center - offset_y) as i32;
+    let x_end = (x_center + offset_x) as i32;
+    let y_end = (y_center + offset_y) as i32;
     let snake_pos = rng.random_pos(x_start..=x_end, y_start..=y_end);
 
     let snake_struct = Snake::new(
@@ -288,6 +307,7 @@ fn spawn_snake(
         &mut commands,
         &mut meshes,
         &mut materials,
+        &mut rng,
         &board,
     );
     board.snake = Some(dbg!(commands.spawn(snake_struct).id()));
@@ -312,7 +332,7 @@ fn grow_apple(
         HashSet::new()
     };
     let apple_pos = loop {
-        let maybe_pos = rng.random_pos(0..board.cols(), 0..board.rows());
+        let maybe_pos = rng.random_pos(0..(board.cols() as i32), 0..(board.rows() as i32));
         if !conflicting_tiles.contains(&maybe_pos) {
             break maybe_pos;
         }
@@ -332,10 +352,118 @@ fn grow_apple(
 // Now add Direction to Snake/Snake Segments and try to move the snake.
 // Right now I believe that involves first moving all the snake segments in the Board resource,
 // And second (potentially a separate system) - moving transform components (so that the movement reflects on the screen).
-// POTENTIAL GOTCHAS: When does user input come in? Is it a separate system or can be done in move_snake() as well? Maybe look at Bevy observers? In that case, maybe movement just changes head direction, and then we go from there.
-// Does that mean that each segment has its own movement direction? If so, when do segment directions change? Automatically somehow, or do I need to update segment directions manually as well?
-fn move_snake() {
-    error!("move_snake()????");
+// Does each segment has its own movement direction? If so, when do segment directions change? Automatically somehow, or do I need to update segment directions manually as well?
+
+// TODO: Develop two chained systems.
+// 1. First one figures out and updates directions of all current snake segments.
+// 2. Second one that runs in chain after the first one, moves all segments into stated directions starting with the head and going further back until the end of the snake.
+// NOTE: MOVEMENT RULES:
+// - If segment is head, move in the current snake direction.
+// - If segment is non-head, move in the direction where the previous (closer-to-head) segment just was.
+
+// REFACTOR: IMPORTANT: Just move the TAIL into where the HEAD Is supposed to go!! That's it????
+fn update_segment_directions(
+    board: Res<Board>,
+    snake_query: Query<&Snake>,
+    mut segment_query: Query<&mut SnakeSegment>,
+) {
+    if let Some(snake_ent) = board.snake {
+        let snek = snake_query.get(snake_ent).unwrap();
+        let mut current_segment = snek.segment_by_pos(snek.head_pos);
+        let mut directions_to_update: Vec<(Entity, Direction)> = vec![];
+
+        loop {
+            let current_segment_ref = segment_query.get(current_segment).unwrap();
+
+            let dir = if let Some(prev_pos) = current_segment_ref.prev {
+                let segment_ent = snek.segment_by_pos(prev_pos);
+                let prev_segment = segment_query.get(segment_ent).unwrap();
+                prev_segment.direction.clone()
+            } else {
+                snek.direction.clone()
+            };
+            directions_to_update.push((current_segment, dir));
+
+            if let Some(next_pos) = current_segment_ref.next {
+                current_segment = *snek.snake_segments.get(&next_pos).unwrap();
+            } else {
+                break;
+            }
+        }
+
+        for (entity, direction) in directions_to_update {
+            segment_query.get_mut(entity).unwrap().direction = direction;
+        }
+    }
+}
+
+fn move_board_pos(pos: &BoardPos, delta: (i32, i32)) -> BoardPos {
+    BoardPos {
+        x: pos.x + delta.0,
+        y: pos.y + delta.1,
+    }
+}
+
+fn move_snake_segments(
+    board: Res<Board>,
+    mut snake_query: Query<&mut Snake>,
+    mut segment_query: Query<&mut SnakeSegment>,
+    mut segment_transform_q: Query<&mut Transform, With<SnakeSegment>>,
+) {
+    if let Some(snake_ent) = board.snake {
+        let mut snek = snake_query.get_mut(snake_ent).unwrap();
+        for mut segment in segment_query.iter_mut() {
+            let segment_entity = snek.segment_by_pos(segment.pos);
+            let new_pos = move_board_pos(&segment.pos, segment.direction.to_delta());
+            let new_world_pos = board_pos_to_world(&new_pos, &board);
+
+            segment.pos = new_pos;
+            snek.snake_segments.remove(&segment.pos);
+            snek.snake_segments.insert(new_pos, segment_entity);
+
+            let mut t = segment_transform_q.get_mut(segment_entity).unwrap();
+            t.translation.x = new_world_pos.0;
+            t.translation.y = new_world_pos.1;
+        }
+    }
+}
+
+fn monitor_game_over(board: Res<Board>, segments_q: Query<&SnakeSegment>) {
+    for segment in segments_q.iter() {
+        if segment.pos.x < 0
+            || segment.pos.x >= board.cols() as i32
+            || segment.pos.y < 0
+            || segment.pos.y >= board.rows() as i32
+        {
+            error!("Sorry, Game Over! You Lost ^_^");
+            std::process::exit(0);
+        }
+    }
+}
+
+// REFACTOR: think about replacing MaximizeShenanigans with a complex PreInit state that includes maximization + snake spawning. Only when the window is maximized + snake has been spawned, we can conclude that the Init has finished and change state to GameRunning.
+
+// TODO: this works, but it allows snake to move into the exact opposite direciton without turning around. This needs to be forbidden.
+fn change_snake_direction(
+    keypress: Res<ButtonInput<KeyCode>>,
+    board: Res<Board>,
+    mut snake: Query<&mut Snake>,
+) {
+    if let Some(snake_ent) = board.snake {
+        let mut snek = snake.get_mut(snake_ent).unwrap();
+        if keypress.just_pressed(KeyCode::ArrowUp) || keypress.just_pressed(KeyCode::KeyW) {
+            snek.direction = Direction::Up;
+        } else if keypress.just_pressed(KeyCode::ArrowRight) || keypress.just_pressed(KeyCode::KeyD)
+        {
+            snek.direction = Direction::Right;
+        } else if keypress.just_pressed(KeyCode::ArrowDown) || keypress.just_pressed(KeyCode::KeyS)
+        {
+            snek.direction = Direction::Down;
+        } else if keypress.just_pressed(KeyCode::ArrowLeft) || keypress.just_pressed(KeyCode::KeyA)
+        {
+            snek.direction = Direction::Left
+        }
+    }
 }
 
 fn main() {
@@ -344,7 +472,7 @@ fn main() {
         .insert_resource(GameRng::new())
         .insert_resource(Board::new())
         .insert_resource(MainWindowDesc::new())
-        .insert_resource(Time::<Fixed>::from_seconds(0.167))
+        .insert_resource(Time::<Fixed>::from_seconds(0.5))
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 resolution: (800, 600).into(),
@@ -366,7 +494,22 @@ fn main() {
                 .run_if(apple_missing)
                 .after(spawn_snake),
         )
-        .add_systems(FixedUpdate, move_snake.run_if(is_maximized))
+        .add_systems(
+            Update,
+            change_snake_direction
+                .run_if(is_maximized)
+                .after(spawn_snake),
+        )
+        .add_systems(
+            FixedUpdate,
+            (
+                update_segment_directions,
+                move_snake_segments,
+                monitor_game_over,
+            )
+                .chain()
+                .run_if(is_maximized),
+        )
         .add_systems(Update, draw_grid_lines)
         .run();
 }
